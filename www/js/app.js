11c6c3f402bbc25a7054c25adc1f9f0605a742ae
danding <1291248710@qq.com>
2016-08-28 14:30:56 +0800

    xx

--- www/js/app.js
+++ www/js/app.js
@@ -1,24 +1,80 @@
-// Ionic Starter App
+angular.module('app',['ionic','ui.router','ngCordova', 'ionic-datepicker'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
+  .run(function($ionicPlatform,$location,$rootScope,$ionicHistory) {
+
+    $ionicPlatform.ready(function() {
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    });
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+  })
+
+  .config(function (ionicDatePickerProvider) {
+    var datePickerObj = {
+      inputDate: new Date(),
+      setLabel: 'Set',
+      todayLabel: 'Today',
+      closeLabel: 'Close',
+      mondayFirst: false,
+      weeksList: ["S", "M", "T", "W", "T", "F", "S"],
+      monthsList: ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"],
+      templateType: 'popup',
+      from: new Date(1900, 8, 1),
+      to: new Date(2060, 8, 1),
+      showTodayButton: true,
+      dateFormat: 'dd MMMM yyyy',
+      closeOnSelect: false,
+      disableWeekdays: [6]
+    };
+    ionicDatePickerProvider.configDatePicker(datePickerObj);
+  })
+
+  .config(function($stateProvider,$urlRouterProvider,$ionicConfigProvider){
+
+    $ionicConfigProvider.platform.ios.tabs.style('standard');
+    $ionicConfigProvider.platform.ios.tabs.position('bottom');
+    $ionicConfigProvider.platform.android.tabs.style('standard');
+    $ionicConfigProvider.platform.android.tabs.position('standard');
+
+    $ionicConfigProvider.platform.ios.navBar.alignTitle('center');
+    $ionicConfigProvider.platform.android.navBar.alignTitle('left');
+
+    $ionicConfigProvider.platform.ios.backButton.previousTitleText('').icon('ion-ios-arrow-thin-left');
+    $ionicConfigProvider.platform.android.backButton.previousTitleText('').icon('ion-android-arrow-back');
+
+    $ionicConfigProvider.platform.ios.views.transition('ios');
+    $ionicConfigProvider.platform.android.views.transition('android');
+
+
+    $stateProvider.state('tabs',{
+      url:'/tabs',
+      abstract:true,
+      templateUrl:'views/tabs/tabs.html'
+    });
+
+
+    $stateProvider.state('login',{
+      url:'/login',
+      controller: 'loginController',
+      templateUrl:'views/login/login.html'
+    });
+
+    $urlRouterProvider.otherwise('/login');
+
+  })
+  
+
+  .factory('Insurances', function () {
+    return [
+      { company: 'AUD', date: '2015-02-03', detail: 'it is not a big deal' },
+      { company: 'BRL', date: '2015-01-01', detail: 'it is not a big deal' },
+      { company: 'CAD', date: '2015-03-01', detail: 'it is not a big deal' },
+      { company: 'CNY', date: '2015-07-01', detail: 'it is a big deal'}
+    ];
+  })
+
+
+  .controller('LeftMenuController',function($scope){
+
+  })
+
