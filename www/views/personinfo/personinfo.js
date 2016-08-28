0a287572ea058cc4bd70172fc12386f3a873c4b8
apple-1 <651090371@qq.com>
2016-08-28 16:43:49 +0800

    1

--- www/views/personinfo/personinfo.js
+++ www/views/personinfo/personinfo.js
@@ -1,5 +1,14 @@
 angular.module('app')
   .controller('personInfoController',function($scope,$state,$http, $location){
 
+    $scope.items= [
+      {id:"1",property:"",num:"太平洋寿险"},
+      {id:"2",property:"保额",num:"400$"},
+      {id:"3",property:"保费",num:"500$"},
+      {id:"4",property:"保险期间",num:"保至70周岁"},
+      {id:"5",property:"缴费期间",num:"2周年"},
+      {id:"6",property:"首年保费",num:"30000$"}
+    ];
+
 
   });
